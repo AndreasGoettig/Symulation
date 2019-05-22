bd34a5c20d5c02f7fac23d0c7d6a9be73771fc06
AndreasGoettig <goettig.andreas@gmx.de>
2019-05-22 22:21:25 +0200

    hexmetrics

--- Symulation/Assets/HexMetrics.cs
+++ Symulation/Assets/HexMetrics.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// alle daten die das Hexagon benötigt
+/// </summary>
 public class HexMetrics : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //der innere und äußere kreis aus dem das Hexagon erstellt wird
+    public const float outerRadius = 10f; 
+    public const float innerRadius = outerRadius * 0.866025404f; // laut wikipedia die perfecte abstand zwischen den beiden kreisen. <- sqrt(3)/5
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public static Vector3[] corners = {
+        new Vector3(0f, 0f, outerRadius), // top
+        new Vector3(innerRadius, 0f, 0.5f * outerRadius), // topright
+        new Vector3(innerRadius, 0f, -0.5f * outerRadius), // topleft
+        new Vector3(0f, 0f, -outerRadius), // bottom
+        new Vector3(-innerRadius, 0f, -0.5f * outerRadius), // bottomleft
+        new Vector3(-innerRadius, 0f, 0.5f * outerRadius) // bottomright
+    };
 }
